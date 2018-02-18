2bcd1387217a10f4a8d19b5fb0a99c2686531b8c
milleriishaun <milleriishaun@gmail.com>
2018-02-18 13:36:51 -0800

    Fixed get_domain_name

--- domain.py
+++ domain.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 def get_domain_name(url):
     try:
         # Split the list up to chunk up
-        results = get_sub_domain_name(url).split()
+        results = get_sub_domain_name(url).split('.')
         # get second to last item, and the last item
         return results[-2] + '.' + results[-1]
     except:
